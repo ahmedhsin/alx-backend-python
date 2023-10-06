5ccf2bdf35d568532c9e6cff94b9b78436970006
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2023-10-06 03:56:09 +0200

    add explaintion for functions in 6 5 tasks

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,6 +4,7 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """sum list func"""
     sum: float = 0
     for i in input_list:
         sum = sum + i
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,6 +4,7 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """sum mixed list"""
     sum: float = 0
     for i in mxd_lst:
         sum = sum + i
