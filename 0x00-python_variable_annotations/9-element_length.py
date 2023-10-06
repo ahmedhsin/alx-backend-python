ab1ac90b621423ec589ae52bed415381c69ace91
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2023-10-06 03:57:40 +0200

    fix task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """element_length function with type anotition"""
-from typing import List, Tuple, Sequence
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: Sequence, ) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """apply annotion fn"""
     return [(i, len(i)) for i in lst]
